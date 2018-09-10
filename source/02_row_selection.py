580f3e12512201b2c217bf094a014c2ffa3804ac
Emanuel Fontelles <emanuelfontelles@fisica.ufc.br>
2018-09-10 13:23:15 -0300

    added others tutorials

--- source/02_row_selection.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-
-def test_import():
-	df = pd.read_csv('data/AAPL.csv')
-	print(df[10:21]) # Printing values with indexes from 10 to 20
-
-if __name__ == '__main__':
-	test_import()
-	print("DONE!")
--- source/42_data_from_poloniex.py
+++
@@ -1,8 +0,0 @@
-from poloniex import Poloniex
-
-connection = Poloniex()
-BTC_ETH = connection.returnTicker()#['BTC_ETH']
-
-print(type(BTC_ETH))
-print(BTC_ETH.keys())
-#print(BTC_ETH['high24hr'])
